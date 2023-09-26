28a9ac48aaf989b091fca9b369c50fd96cefd1a2
Kefuoe <kmofoka@ebsint.com>
2023-09-26 07:56:43 +0200

    added client and server folders

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const bodyParser = require('body-parser')
+const db = require('./configs/dbconfig')
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
+
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+
+// app.get('/', (req, res) => {
+//   res.status(200).send('hello from here')
+// }) //route url
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/mines', db.getUsers)
+
+app.listen(port, () =>{
+    console.log(`running on ${port}`)
+});
+
+
---
+++ server/configs/dbconfig.js
@@ -0,0 +1,24 @@
+const {Pool} = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'pcmm_assignment',
+  password: 'Kefuoe@23',
+  port: 5432,
+})
+pool.connect();
+
+
+const getUsers = (request, response) => {
+   pool.query(`Select * from mines`, (error, results) => {
+     if (error) {
+       throw error;
+     }
+     response.status(200).json(results.rows)
+     pool.end;
+   })
+ }
+
+ module.exports = {
+   getUsers,
+ }
